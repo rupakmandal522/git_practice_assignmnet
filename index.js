5b9d885ad8d0454efb5d07b87a047014d13136f8
rupakmandal522 <rupakmandal522@gmail.com>
2022-11-29 20:55:17 +0530

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+let str = "Rupak";
+let bag= "";
+for(let i=str.length-1; i>=0; i--){
+    bag=bag+str[i]
+}
+if(bag!=str){
+    console.log(console.log("true"))
+}
+else{
+  console.log("false");
+}
