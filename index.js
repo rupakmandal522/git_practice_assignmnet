d204db133848f45b3faccd8f52c25c45d92eabb2
rupakmandal522 <rupakmandal522@gmail.com>
2022-12-07 00:24:01 +0530

    add

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let str = "Rupak";
-let bag= "";
-for(let i=str.length-1; i>=0; i--){
-    bag=bag+str[i]
+// let str = "Rupak";
+// let bag= "";
+// for(let i=str.length-1; i>=0; i--){
+//     bag=bag+str[i]
+// }
+// if(bag===str){
+//     console.log(console.log("true"))
+// }
+// else{
+//   console.log("false");
+// }
+
+let num = 13; 
+let count = 0
+for(let i=0; i<=num; i++){
+  if(num%i==0){
+    count++
+  }
 }
-if(bag===str){
-    console.log(console.log("true"))
+if(count<=1){
+  console.log("true")
 }
 else{
   console.log("false");
